4324179711b6f750a246f152915e64cdf84b97db
laixin <522537892@qq.com>
2020-07-02 12:06:46 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -14,7 +15,6 @@ import com.sunnyweather.android.MainActivity
 import com.sunnyweather.android.R
 import com.sunnyweather.android.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
-import androidx.core.widget.addTextChangedListener as addTextChangedListener1
 
 class PlaceFragment : Fragment() {
 
@@ -63,7 +63,7 @@ class PlaceFragment : Fragment() {
                 viewModel.placeList.addAll(places)
                 adapter.notifyDataSetChanged()
             } else {
-                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, "未搜索到任何地点", Toast.LENGTH_SHORT).show()
                         result.exceptionOrNull()?.printStackTrace()
             }
         })
